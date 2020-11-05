6d6e4489b23a1333cbb748259f37d3d4fd0dff4e
kavitha sundarraj <jeyakumar.kavitha@gmail.com>
2020-11-05 09:20:00 +0100

    Add project files.

---
+++ MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*1.	MathOperation
+ * Date : 05 Nov 2020
+ * a class MathOperations, which should have 3 times method Add(). Method Add() have to be invoked with:
+•	Add(int, int): int
+•	Add(double, double, double): double
+•	Add(decimal, decimal, decimal): decimal
+*/
+namespace polymorphism
+{
+    class MathOperations
+    {
+        public int Add (int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        public decimal Add(decimal a, decimal b, decimal c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/*1.	MathOperation
+ * Date : 05 Nov 2020 */
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MathOperation");
+            MathOperations mo = new MathOperations();
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+
+        }
+    }
+}
